11253c7d62f073cd18c6629ad5551978b916df7b
sakinah <sakinahamiri1@gmail.com>
2025-06-07 15:54:14 -0700

    Added Table class with multiplicationTable method

---
+++ src/day01_commit_push_pull/Table.java
@@ -0,0 +1,9 @@
+package day01_commit_push_pull;
+
+public class Table {
+    public static void multiplicationTable(int num){
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(num + " x " + i + " = " + (num*i));
+        }
+    }
+}
