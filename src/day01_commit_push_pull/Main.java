4b99b932bfb3de0eb1ea5d243130cf67f0b5d760
sakinah <sakinahamiri1@gmail.com>
2025-06-07 17:52:00 -0700

    Added multiplicationTable method call to Main class

--- src/day01_commit_push_pull/Main.java
+++ src/day01_commit_push_pull/Main.java
@@ -2,10 +2,14 @@ package day01_commit_push_pull;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, Git!");
 
+        Table.multiplicationTable(5);
+
+        Table.multiplicationTable(7);
+        Table.multiplicationTable(9);
+
+
     }
-
 }
-
-
