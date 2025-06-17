1c8315daf31893d50edb70da1ece76c62b13ea8e
sakinah120 <sakinahamiri1@gmail.com>
2025-06-16 20:14:51 -0700

    Added divide() method to SimpleCalculator.java from GitHub

--- src/day02_pull_clone_rollback/SimpleCalculator.java
+++ src/day02_pull_clone_rollback/SimpleCalculator.java
@@ -12,12 +12,23 @@ package day02_pull_clone_rollback;
         public static int multiply(int a, int b) {
             return a * b;
     }
+        public static double divide(int a, int b) {
+            if (b == 0) {
+            System.out.println("Cannot divide by zero.");
+            return 0;
+        } 
+                return (double) a / b;
+    }
  
 
-        public static void main(String[] args) {
+            public static void main(String[] args) {
             int x = 10, y = 5;
             System.out.println("Add: " + add(x, y));
             System.out.println("Subtract: " + subtract(x, y));
+
+
+
+            
         }
     }
 
