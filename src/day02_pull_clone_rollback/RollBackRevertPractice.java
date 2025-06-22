f7a817ade3ca6fe1cbfc528890962564562bd5a7
sakinah <sakinahamiri1@gmail.com>
2025-06-21 20:02:55 -0700

    Completed task A

---
+++ src/day02_pull_clone_rollback/RollBackRevertPractice.java
@@ -0,0 +1,10 @@
+package day02_pull_clone_rollback;
+
+public class RollBackRevertPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Git day 2 review");
+
+        System.out.println("Completed some task using Approach");
+    }
+}
